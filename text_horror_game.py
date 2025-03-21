ebf3c7dd3d35f14ed0fdf8d5889da09997cba4fc
Roy Hsu <145495716+hsur05@users.noreply.github.com>
2025-03-20 20:45:12 -0700

    Add files via upload

---
+++ text_horror_game.py
@@ -0,0 +1,14 @@
+import time
+
+
+# STORY INTRODUCTION
+print("ONE MORE STEP\n")
+time.sleep(1)
+print("You are driving in rainy rural Rhode Island in the middle of the night, when you hear a loud pop behind the back passenger's seat...")
+#time.sleep(3)
+print("You get out of the car with your phone light to see what's going on, only to notice that not only do you have a flat tire, but there is also no cellular reception... \n")
+
+#time.sleep(5)
+print("You're cold. You're alone. And you don't know what to do...")
+print("You start to feel your heart sink into your stomach. You're breathing fast. You seem to see better, think clearer.")
+
