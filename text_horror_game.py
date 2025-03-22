a82a7085ef7d6564f1fb11307d7387a33b4b2b74
Roy Hsu <145495716+hsur05@users.noreply.github.com>
2025-03-21 22:49:37 -0700

    Update text_horror_game.py

--- text_horror_game.py
+++ text_horror_game.py
@@ -2,13 +2,16 @@ import time
 
 
 # STORY INTRODUCTION
-print("ONE MORE STEP\n")
-time.sleep(1)
-print("You are driving in rainy rural Rhode Island in the middle of the night, when you hear a loud pop behind the back passenger's seat...")
+print("What's Next?\n")
+#time.sleep(1)
+print("I am driving in rainy rural Rhode Island in the middle of the night, when I hear a loud pop behind the back passenger's seat...")
 #time.sleep(3)
-print("You get out of the car with your phone light to see what's going on, only to notice that not only do you have a flat tire, but there is also no cellular reception... \n")
+print("I get out of the car with my phone light to see what's going on, only to notice that not only do you have a flat tire, but there is also no cellular reception... \n")
 
-#time.sleep(5)
-print("You're cold. You're alone. And you don't know what to do...")
-print("You start to feel your heart sink into your stomach. You're breathing fast. You seem to see better, think clearer.")
+#time.sleep(3)
+print("I am cold and alone. And I don't know what to do...")
+print("I start to feel my heart sink into my stomach. my breathing is so fast, but I seem to see better, think clearer.")
 
+#time.sleep(3)
+print("\"What should I do?\" I thought to myself as I paced around the car.")
+print("In the distance, I saw in a forest, about a quarter mile away, that there was a dim light and smoke rising into the sky. Is that a campfire? or a chimney?")
