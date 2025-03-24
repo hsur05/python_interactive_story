d883be1a530ed97dcac124bbd8b3188245df8f55
Roy Hsu <145495716+hsur05@users.noreply.github.com>
2025-03-24 10:00:27 -0700

    Update text_horror_game.py

--- text_horror_game.py
+++ text_horror_game.py
@@ -1,4 +1,5 @@
 import time
+import random 
 
 
 # STORY INTRODUCTION
@@ -6,11 +7,11 @@ print("What's Next?\n")
 #time.sleep(1)
 print("I am driving in rainy rural Rhode Island in the middle of the night, when I hear a loud pop behind the back passenger's seat...")
 #time.sleep(3)
-print("I get out of the car with my phone light to see what's going on, only to notice that not only do you have a flat tire, but there is also no cellular reception... \n")
+print("I get out of the car with my phone light to see what's going on, only to notice that not only do I have a flat tire, but there is also no cellular reception... \n")
 
 #time.sleep(3)
 print("I am cold and alone. And I don't know what to do...")
-print("I start to feel my heart sink into my stomach. my breathing is so fast, but I seem to see better, think clearer.")
+print("I start to feel my heart sink into my stomach. my breathing is so fast, but I seem to see better, think clearer.\n")
 
 #time.sleep(3)
 print("\"What should I do?\" I thought to myself as I paced around the car.")
