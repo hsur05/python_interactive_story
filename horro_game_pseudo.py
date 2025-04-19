032b720e5e4c142296dccf98bc64536913142c7d
Roy Hsu <145495716+hsur05@users.noreply.github.com>
2025-04-18 21:15:36 -0700

    Add files via upload

---
+++ horro_game_pseudo.py
@@ -0,0 +1,40 @@
+"""
+
+Class Character()
+- inventory tracking 
+- health tracking
+- choose item/thing
+
+
+Class Choices()?
+- allows user to choose things, such as rooms, items/clues pick up
+- method in the Character class instead
+
+
+Class Rooms()
+- description/narrative of room
+- choose clues()
+- choose room() 
+- optional: use item in the room
+
+
+PARENT class ThingInspect?: (avoid for now unless very repetitive)
+
+Class Items()
+- should i pick up? y/n
+- item, item description 
+- update inventory in Character()
+
+Class Clues()
+- should I inspect? y/n
+- description of clue
+- optional: explore other clues in the room? y/n
+- optional: end of story
+
+
+Class Gameplay()- not as necessary, just a function is enough to start
+- main gameplay, leads to all other classes from here
+- move player tp dofferemt rooms
+- continue_game() ? 
+- game over
+"""
